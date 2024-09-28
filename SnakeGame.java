7935ef25e37567974c5d3e00ba3b31508a9aee77
mishrahappy <mishrahappy045@gmail.com>
2024-09-28 16:22:50 +0530

    comment

--- SnakeGame.java
+++ SnakeGame.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
+//comment
+
 public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     private class Tile {
         int x;
